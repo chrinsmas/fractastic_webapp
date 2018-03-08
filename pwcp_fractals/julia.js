d69157922164fd23beb7aa602f2264b243cbd875
Keith Pearson <keith@qubitrot.org>
2018-03-08 08:47:20 -0800

    refactor coloring cod

--- pwcp_fractals/julia.js
+++ pwcp_fractals/julia.js
@@ -3,6 +3,11 @@ class JuliaRenderer extends PointwiseCPFractalRenderer
     constructor(spec) {
         spec.escape_radius = 2.0; //all points further than 2 from the origin
                                   //cannot be in the julia set.
+        
+        let dec = ["#798CFF", "#74CDFB", "#6FF8E1", "#6AF598", "#81F165",
+                   "#C5EE60", "#EBCB60", "#E77D57", "#E45277", "#E14EBE",
+                   "#B54ADE"];
+        spec.exterior_colors = spec.exterior_colors || dec;
 
         spec.frags_src = `
             precision highp float;
@@ -12,7 +17,7 @@ class JuliaRenderer extends PointwiseCPFractalRenderer
             uniform int max_iter;
             uniform vec2 c;
 
-            void iterate(inout vec2 z) {
+            void iterate(inout vec2 z, in vec2 c_whatever) {
                 vec2 tmp;
                 tmp.x = z.x*z.x - z.y*z.y + c.x;
                 tmp.y = 2.0*z.x*z.y       + c.y;
@@ -20,14 +25,9 @@ class JuliaRenderer extends PointwiseCPFractalRenderer
             }
 
             vec4 color(in vec2 z) {
-                for (int i=0; i<100000; ++i) {
-                    iterate(z);
-                    if (length(z) > escape_radius) {
-                        float b = exp(-0.01*sqrt(view_scale)*float(i));
-                        return vec4(0.5*b,0.7*b,0.6*b,1);
-                    } else if (i > max_iter) {
-                        break;
-                    }
+                for (int i=0; i<100; ++i) {
+                    ${PointwiseCPFractalRenderer.unrolled_color_loop(
+                                                      spec.exterior_colors)}
                 }
                 return vec4(0,0,0,1);
             }
