2f7380111a6d0e059706a64cb4f331639526c9a2
Keith Pearson <keith@qubitrot.org>
2018-03-14 09:05:51 -0700

    custom coloring support

--- pwcp_fractals/julia.js
+++ pwcp_fractals/julia.js
@@ -3,13 +3,41 @@ class JuliaRenderer extends PointwiseCPFractalRenderer
     constructor(spec) {
         spec.escape_radius = 2.0; //all points further than 2 from the origin
                                   //cannot be in the julia set.
+
+        spec.max_iter = spec.max_iter || 1000;
         
         let dec = ["#798CFF", "#74CDFB", "#6FF8E1", "#6AF598", "#81F165",
                    "#C5EE60", "#EBCB60", "#E77D57", "#E45277", "#E14EBE",
                    "#B54ADE"];
         spec.exterior_colors = spec.exterior_colors || dec;
 
-        spec.frags_src = `
+        spec.frags_src = JuliaRenderer.create_frags_src(spec.exterior_colors, 
+                                                        spec.max_iter);
+
+        super(spec); //call PointwiseCPFractalRenderer constructor
+
+        this.c = spec.c || [-0.4,0.6];
+        this.max_iter = 12;
+    }
+
+    render() {
+        //PointwiseCPFractalRenderer does not include this uniform by default.
+        this.u_c = this.gl.getUniformLocation(this.gl_program, "c");
+        this.gl.uniform2fv(this.u_c, this.c);
+
+        //default renderer can handle the rest
+        super.render();
+    }
+   
+    set exterior_colors(colors) {
+        this.frags_src = JuliaRenderer.create_frags_src(colors, this.max_iter);
+        this.gl_program = this.build_program(this.vertexs_src,
+                                             this.frags_src);
+    } 
+
+    static create_frags_src(exterior_colors, max_iter) 
+    {
+        let frags_src = `
             precision highp float;
             uniform mat4 view_transform;
             uniform float view_scale;
@@ -25,9 +53,8 @@ class JuliaRenderer extends PointwiseCPFractalRenderer
             }
 
             vec4 color(in vec2 z) {
-                for (int i=0; i<100; ++i) {
-                    ${PointwiseCPFractalRenderer.unrolled_color_loop(
-                                                      spec.exterior_colors)}
+                for (int i=0; i<${Math.ceil(max_iter/exterior_colors.length)}; ++i) {
+                    ${PointwiseCPFractalRenderer.unrolled_color_loop(exterior_colors)}
                 }
                 return vec4(0,0,0,1);
             }
@@ -40,17 +67,7 @@ class JuliaRenderer extends PointwiseCPFractalRenderer
             }
         `;
 
-        super(spec); //call PointwiseCPFractalRenderer constructor
-
-        this.c = spec.c || [-0.4,0.6];
+        return frags_src;
     }
 
-    render() {
-        //PointwiseCPFractalRenderer does not include this uniform by default.
-        this.u_c = this.gl.getUniformLocation(this.gl_program, "c");
-        this.gl.uniform2fv(this.u_c, this.c);
-
-        //default renderer can handle the rest
-        super.render();
-    }
 };
